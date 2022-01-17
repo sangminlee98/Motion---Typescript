3f037ebde0da278e28f68cdbd70ff4e2a2babb13
sangminlee98 <tkdals0975@naver.com>
2022-01-17 18:49:27 +0900

    [modify] imageComponent

--- src/components/page/item/image.ts
+++ src/components/page/item/image.ts
@@ -1,12 +1,13 @@
 export class ImageComponent {
+    private readonly htmlTemplateString = `
+    <li class="image">
+        <div class="image__holder"><img class="image__thumbnail"></div>
+        <p class="image__title"></p>
+    </li>`;
     private element: HTMLLIElement;
     constructor(title: string, url: string) {
         const template = document.createElement('template');
-        template.innerHTML = `
-        <li class="image">
-            <div class="image__holder"><img class="image__thumbnail"></div>
-            <p class="image__title"></p>
-        </li>`;
+        template.innerHTML = this.htmlTemplateString;
         this.element = template.content.firstElementChild! as HTMLLIElement;
         const imageElement = this.element.querySelector('.image__thumbnail')! as HTMLImageElement;
         imageElement.src = url;
